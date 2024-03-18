031c6b78236dd8525f83b8c0c3920a0ea06eb7ae
TiSoares8 <tiagosoares.3103@gmail.com>
2024-03-18 08:30:30 -0300

    Formularios

---
+++ projeto-inicial/scripts.js
@@ -0,0 +1,7 @@
+var botao = document.getElementById("botao");
+botao.addEventListener("click", alertar);
+
+var nome = document.getElementById("nome");
+function alertar(){
+    alert("O nome inserido foi " + nome.value);
+}
--- scripts.js
+++ scripts.js
@@ -1,6 +1,34 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar);
 
+
+var nome = document.getElementById("nome");
+var saida = document.getElementById("saida-de-dados");
+var email = document.getElementById("email");
+var telefone = document.getElementById("telefone");
+var cep = document.getElementById("cep");
+var logradouro = document.getElementById("logradouro");
+var numero = document.getElementById("numero");
+var complemento = document.getElementById("compl");
+var bairro = document.getElementById("bairro");
+var cidade = document.getElementById("cidade");
+var estado = document.getElementById("estado");
+
+
+
+
+
 function alertar(){
-    alert("O botão foi clicado!!!");
+// alert("O nome inserido foi" + nome.value);
+    saida.innerText ="Nome:" + nome.value+
+    "\n E-mail:" + email.value +
+    "\n Telefone:" + telefone.value +
+    "\n CEP:" + cep.value +
+    "\n Logradouro:" + logradouro.value +
+    "\n Número:" + numero.value +
+    "\n Complemento:" + compl.value +
+    "\n Bairro:" + bairro.value +
+    "\n Cidade:" + cidade.value +
+    "\n Estado:" + estado.value;
+
 }
